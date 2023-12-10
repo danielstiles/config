658ee27f3dcc4ae05eb662ccc5e6af1b0dc905aa
Daniel Stiles <22626981+danielstiles@users.noreply.github.com>
2023-12-10 10:51:27 -0500

    feat: install git in git.bash

--- bootstrap/git.bash
+++ bootstrap/git.bash
@@ -6,7 +6,7 @@ if [ -z $GCM_VERSION ]; then
 fi
 
 sudo apt-get update
-sudo apt-get install -y keychain
+sudo apt-get install -y keychain git-all
 wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v$GCM_VERSION/gcm-linux_amd64.$GCM_VERSION.tar.gz
 sudo tar -xvf gcm-linux_amd64.$GCM_VERSION.tar.gz -C /usr/local/bin
 git-credential-manager configure
